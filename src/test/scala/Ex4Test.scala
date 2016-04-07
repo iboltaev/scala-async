a5078c41cb3845f30891b37bc7de65ddfbe91a01
Ilya Boltaev <iboltaev@yandex-team.ru>
2016-04-07 21:33:00 +0300

    moved monad variable into the test function

--- src/test/scala/Ex4Test.scala
+++ src/test/scala/Ex4Test.scala
@@ -14,12 +14,12 @@ class Ex4Spec extends FlatSpec with Matchers {
   import Ex3._
   import Ex2._
 
-  implicit val m = new FStateMonad[Int]
-
   def makeStream(l: List[Int]) = genAsyncStream(l)(
     l => if (l.isEmpty) new NoFuture else Future((l.head, l.tail)))
 
   it should "work with 'foreach'" in {
+    implicit val m = new FStateMonad[Int]
+    
     val fstate = for (
       _ <- foreach(makeStream(0 :: 1 :: 2 :: Nil)) {
         v => m.mods(_ + 1)
